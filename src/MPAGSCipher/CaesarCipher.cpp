fcd79665201850842569fac55643abfbb3d8e030
WillR224 <will.rose.224@gmail.com>
2026-03-13 12:23:40 +0000

    Add Multithreading

--- src/MPAGSCipher/CaesarCipher.cpp
+++ src/MPAGSCipher/CaesarCipher.cpp
@@ -1,6 +1,7 @@
 #include "CaesarCipher.hpp"
 #include "Alphabet.hpp"
-
+#include <future>
+#include <chrono>
 #include <string>
 
 /**
@@ -44,35 +45,66 @@ std::string CaesarCipher::applyCipher(const std::string& inputText,
 {
     // Create the output string
     std::string outputText;
-
-    // Loop over the input text
-    char processedChar{'x'};
-    for (const auto& origChar : inputText) {
-        // For each character in the input text, find the corresponding position in
-        // the alphabet by using an indexed loop over the alphabet container
-        for (std::size_t i{0}; i < Alphabet::size; ++i) {
-            if (origChar == Alphabet::alphabet[i]) {
-                // Apply the appropriate shift (depending on whether we're encrypting
-                // or decrypting) and determine the new character
-                // Can then break out of the loop over the alphabet
-                switch (cipherMode) {
-                    case CipherMode::Encrypt:
-                        processedChar =
-                            Alphabet::alphabet[(i + key_) % Alphabet::size];
-                        break;
-                    case CipherMode::Decrypt:
-                        processedChar =
-                            Alphabet::alphabet[(i + Alphabet::size - key_) %
-                                               Alphabet::size];
-                        break;
-                }
-                break;
-            }
+    int threadCount{5};
+    std::vector<std::future<std::string>> futures;
+    std::string inputTextChunk;
+    for(int i{0}; i < threadCount; ++i) {
+        int chunkSize = inputText.size() / threadCount;
+        int start = i * chunkSize;
+        int end;
+        if(i == threadCount -1){
+            end = inputText.size();
+        }
+        else{
+            end = (i + 1) * chunkSize;
         }
 
-        // Add the new character to the output text
-        outputText += processedChar;
+        auto processChunk = [this, cipherMode](const std::string& textChunk) {
+            // Loop over the input text
+            std::string outputChunk;
+            char processedChar{'x'};
+            for (const auto& origChar : textChunk) {
+                // For each character in the input text, find the corresponding position in
+                // the alphabet by using an indexed loop over the alphabet container
+                for (std::size_t j{0}; j < Alphabet::size; ++j) {
+                    if (origChar == Alphabet::alphabet[j]) {
+                        // Apply the appropriate shift (depending on whether we're encrypting
+                        // or decrypting) and determine the new character
+                        // Can then break out of the loop over the alphabet
+                        switch (cipherMode) {
+                            case CipherMode::Encrypt:
+                                processedChar =
+                                    Alphabet::alphabet[(j + key_) % Alphabet::size];
+                                break;
+                            case CipherMode::Decrypt:
+                                processedChar =
+                                    Alphabet::alphabet[(j + Alphabet::size - key_) %
+                                                    Alphabet::size];
+                                break;
+                        }
+                        break;
+                    }
+                }
+                // Add the new character to the output text
+                outputChunk += processedChar;
+            }
+            return outputChunk;
+        };
+
+        futures.push_back(std::async(std::launch::async, processChunk, inputText.substr(start, end - start)));
+
+
     }
+
+    for (auto& future : futures) {
+        std::future_status status{std::future_status::ready};
+        do {
+            status = future.wait_for(std::chrono::seconds(1));
+        } 
+        while (status != std::future_status::ready);
+        outputText += future.get();
+    }
+
 
     return outputText;
 }
